4d36f91e9a8bc6170273ea9611b61231c54ca34a
Bruno <brunofas12@gmail.com>
2022-01-09 12:05:16 +0800

    refactor: Analyzing some codes

--- components/earth/index.js
+++ components/earth/index.js
@@ -7,6 +7,7 @@ import { useTexture } from '@react-three/drei'
 
 import CloudsMap from './earth-assets/8k_earth_clouds.jpg'
 import ColorMap from './earth-assets/8k_earth_normal_map.jpg'
+
 import SpecularMap from './earth-assets/8k_earth_specular_map.jpg'
 import NightMap from './earth-assets/8k_earth_nightmap.jpg'
 import DayMap from './earth-assets/8k_earth_daymap.jpg'
@@ -15,7 +16,7 @@ import DayMap from './earth-assets/8k_earth_daymap.jpg'
 const Earth = () => {
 
 
-    const colorMap = useLoader(TextureLoader, './earth-assets/8k_earth_normal_map.jpg')
+    const colorMap = useLoader(TextureLoader, 'ColorMap')
 
     return (
         <>
@@ -24,7 +25,7 @@ const Earth = () => {
         <mesh>
             <sphereGeometry attach="geometry" args={[1.57, 32, 32]} />
             <meshStandardMaterial attach="material"  />
-            <meshStandardMaterial map={colorMap} />
+            <meshStandardMaterial map={colorMap}  />
         </mesh>
             
         </>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import dynamic from 'next/dynamic'
 import styles from '../styles/Home.module.css'
 import tw from 'tailwind-styled-components'
 
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import Earth  from "../components/earth";
+// import Earth  from "../components/earth";
+
+
+const Earth = dynamic(() => import('../components/earth'), { ssr: false})
 
 
 
